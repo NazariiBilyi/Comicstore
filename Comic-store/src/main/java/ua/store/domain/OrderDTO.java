c23833b954b6ceca64c075619f4a4801050f37c7
Nozgot1988 <nazarbiliy@gmail.com>
2018-09-13 11:35:53 +0300

    Another minor changes

--- Comic-store/src/main/java/ua/store/domain/OrderDTO.java
+++ Comic-store/src/main/java/ua/store/domain/OrderDTO.java
@@ -1,5 +1,26 @@
 package ua.store.domain;
 
-public class OrderDTO {
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ua.store.entity.ProductEntity;
+
+@Data
+@NoArgsConstructor
+public class OrderDTO {
+	
+	private Long id;
+	
+	private String orderId;
+	
+	private BigDecimal orderPrice;
+	
+	private LocalDate orderDate;
+	
+	private List<ProductEntity> products;
+	
+	private Integer orderItemsNumber;
 }
